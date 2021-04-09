9f8206144e1db84e6638c31fc4a7b377f97ec698
PhamQuocHuy12 <71309596+PhamQuocHuy12@users.noreply.github.com>
2021-04-09 19:13:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firenotes/AddNote.java
@@ -0,0 +1,74 @@
+package com.example.firenotes;
+
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddNote extends AppCompatActivity {
+    FirebaseFirestore fstore;
+    EditText noteTitle, noteContent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fstore = FirebaseFirestore.getInstance();
+        noteContent = findViewById(R.id.addNoteContent);
+        noteTitle = findViewById(R.id.addNoteTitle);
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nTitle = noteTitle.getText().toString();
+                String nContent = noteContent.getText().toString();
+
+                if(nTitle.isEmpty() || nContent.isEmpty()){
+                    Toast.makeText(AddNote.this, "EMPTY", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                DocumentReference docref = fstore.collection("notes").document();
+                Map<String, Object> note = new HashMap<>();
+                note.put("title", nTitle);
+                note.put("content", nContent);
+
+                docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(AddNote.this, "Note added", Toast.LENGTH_SHORT).show();
+                        onBackPressed();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddNote.this, "Error try again", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
